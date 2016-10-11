57a66e7c736afffca02e2aeed4be9eca1f068d1d
xenteros <adam@gurgul.com.pl>
2016-10-11 14:29:57 +0200

    start.spring.io

---
+++ src/main/java/pl/com/gurgul/GabinetLekarskiApplication.java
@@ -0,0 +1,12 @@
+package pl.com.gurgul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GabinetLekarskiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GabinetLekarskiApplication.class, args);
+	}
+}
