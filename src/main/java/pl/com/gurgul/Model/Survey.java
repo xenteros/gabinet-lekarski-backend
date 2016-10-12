b87cce2d36965f266f112def56d5800fc393d730
xenteros <adam@gurgul.com.pl>
2016-10-12 08:51:46 +0200

    connect no ssl

--- src/main/java/pl/com/gurgul/Model/Survey.java
+++ src/main/java/pl/com/gurgul/Model/Survey.java
@@ -1,17 +1,47 @@
-package pl.com.gurgul.Model;
+package pl.com.gurgul.model;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 /**
  * Created by agurgul on 11.10.2016.
  */
 
 @Entity
-@Table(name = "surveys")
+@Table(name = "Surveys")
 public class Survey {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
+    @NotNull
+    private String username;
+
+    public Survey(String username) {
+        this.username = username;
+    }
+
+    public Survey(long id) {
+        this.id = id;
+    }
+
+    public Survey() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 }
